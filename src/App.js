9c29081a16b1c4ebce128fdc6660cde21b3c576e
MaksimLisovoi <lisovoi123@gmail.com>
2021-03-15 15:22:11 +0200

    init: start project and add searchbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Searchbar from "./components/Searchbar";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    const {} = this.state;
+
+    return (
+      <>
+        <Searchbar />
+      </>
+    );
+  }
+}
+export default App;
